b2d56e2e1057147f7983f750f8ee7154d19aa410
Reed Allen <allenreed255@gmail.com>
2025-10-06 13:39:01 -0500

    Add files via upload

---
+++ assignment-2/assignment-2/site.js
@@ -0,0 +1,13 @@
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+let welcomeMessage = '' // default message
+
+if (isMorning) {welcomeMessage = 'Good Morning!'}
+if (isAfternoon) {welcomeMessage = 'Good Afternoon!'}   
+if (isEvening) {welcomeMessage = 'Good Evening!'}
+
+document.querySelector('#welcome').textContent = welcomeMessage // display the message in the div with id 'welcome'
